86e0e31bb0ded10c178c315295ad5b9d11c3a5a1
Nirav-Sanepara <nrsanepara@gmail.com>
2024-03-04 18:55:36 +0530

    half-done

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -9,7 +9,7 @@ import { addRegisterUser } from "../Slices/userSlice";
 import axios from "axios";
 
 
-const RegisterScreen = ({ location, history }) => {
+const RegisterScreen = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
