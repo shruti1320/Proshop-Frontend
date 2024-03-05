421f419fbeff95770ce030a53c6b41062fe22ff8
Nirav-Sanepara <nrsanepara@gmail.com>
2024-03-05 19:12:21 +0530

    seperation1

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -6,11 +6,17 @@ function AccountScreen() {
   return (
     <div>
       <p>Account Screen</p>
-      
+
       <Navbar className="linkstyle">
-        <Nav.Link href="/cart" className="border border-2" >Cart</Nav.Link>
-        <Nav.Link href="/profile" className="border border-2">Account</Nav.Link>
-        <Nav.Link href="/order" className="border border-2">My Order</Nav.Link>
+        <Nav.Link href="/cart" className="border border-2">
+        <i class="fa-solid fa-cart-shopping"></i>Cart
+        </Nav.Link>
+        <Nav.Link href="/profile" className="border border-2 ms-2">
+        <i class="fa-solid fa-user"></i>Account
+        </Nav.Link>
+        <Nav.Link href="/order" className="border border-2 ms-2">
+        <i class="fa-solid fa-cube"></i>My Order
+        </Nav.Link>
       </Navbar>
     </div>
   );
