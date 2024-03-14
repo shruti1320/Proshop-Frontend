f766f1b51d42b31260dd676e1d696adfc01a3d6e
Nirav-Sanepara <nrsanepara@gmail.com>
2024-03-14 19:21:20 +0530

    count in stock condition

--- src/datatable/MerchantPage/index.js
+++ src/datatable/MerchantPage/index.js
@@ -314,7 +314,7 @@ export default function MerchantPageProductDetails({ props }) {
 
               }}
             >
-              <Tooltip title="Edit">
+              <Tooltip title="Edit" sx={{color:"black", backgroundColor:"white"}}>
                 <IconButton
                   onClick={(e) => {
                     e.stopPropagation()
