a3434a3a54d1b8d409bd2b2d9f7f53a01aab31f9
Nirav-Sanepara <nrsanepara@gmail.com>
2024-03-26 14:52:57 +0530

    shruti side change

--- src/componant/HomeScreen/Filter.js
+++ src/componant/HomeScreen/Filter.js
@@ -1,108 +1,108 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import { Range } from "react-range";
-import { useDispatch } from "react-redux";
-import { setFilteredProducts } from "../../Slices/productSlice";
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+// import { useState } from "react";
+// import Button from "react-bootstrap/Button";
+// import Offcanvas from "react-bootstrap/Offcanvas";
+// import { Range } from "react-range";
+// import { useDispatch } from "react-redux";
+// import { setFilteredProducts } from "../../Slices/productSlice";
+// import { Link } from "react-router-dom";
+// import { useTranslation } from "react-i18next";
 
-function Example() {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const { t } = useTranslation("global");
+// function Example() {
+//   const [show, setShow] = useState(false);
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+//   const { t } = useTranslation("global");
 
 
-  const dispatch = useDispatch();
-  const [priceRange, setPriceRangeChange] = useState([20, 10000]);
+//   const dispatch = useDispatch();
+//   const [priceRange, setPriceRangeChange] = useState([20, 10000]);
 
-  const handleRangeChange = (newPriceRange) => {
-    setPriceRangeChange(newPriceRange);
-  };
+//   const handleRangeChange = (newPriceRange) => {
+//     setPriceRangeChange(newPriceRange);
+//   };
 
-  const handleFilterButtonClick = () => {
-    dispatch(setFilteredProducts(priceRange));
-    handleClose();
-  };
-  return (
-    <>
-      <button
-        type="button"
-        onClick={handleShow}
-        className="btn b-secondary btn-outline-secondary border-start-0 bg-transparent text-dark"
-      >
-      {t("homePage.filter")}
-      </button>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title className="fs-3 mt-3">
-            <div>Product Categories</div>
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Link to="/airpodsScreen" className="text-danger text-decoration-none">
-            AirPods
-          </Link>
-          <br></br>
-          <Link to="/cameraScreen" className="text-danger text-decoration-none">
-            Camera
-          </Link>
-          <br></br>
-          <Link to="/smartphoneScreen" className="text-danger text-decoration-none">
-            Smart Phone
-          </Link>
-          <div className="mt-5 fs-2">Filter by price</div>
-          <div className="mt-3">
-            <Range
-              step={100}
-              min={20}
-              max={10000}
-              values={priceRange}
-              onChange={handleRangeChange}
-              renderTrack={({ props, children }) => (
-                <div
-                  {...props}
-                  style={{
-                    ...props.style,
-                    height: "5px",
-                    width: "100%",
-                    backgroundColor: "#ccc",
-                  }}
-                >
-                  {children}
-                </div>
-              )}
-              renderThumb={({ props }) => (
-                <div
-                  {...props}
-                  style={{
-                    ...props.style,
-                    height: "17px",
-                    width: "17px",
-                    backgroundColor: "black",
-                    borderRadius: "50%",
-                  }}
-                />
-              )}
-            />
-          </div>
-          <div>
-            <div className="float-end mt-4">
-              ${priceRange[0]} - ${priceRange[1]}
-            </div>
-            <button
-              type="button"
-              className="btn btn-outline-secondary mt-3 border-start-0"
-              onClick={handleFilterButtonClick}
-            >
-              Filter
-            </button>
-          </div>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
+//   const handleFilterButtonClick = () => {
+//     dispatch(setFilteredProducts(priceRange));
+//     handleClose();
+//   };
+//   return (
+//     <>
+//       <button
+//         type="button"
+//         onClick={handleShow}
+//         className="btn b-secondary btn-outline-secondary border-start-0 bg-transparent text-dark"
+//       >
+//       {t("homePage.filter")}
+//       </button>
+//       <Offcanvas show={show} onHide={handleClose}>
+//         <Offcanvas.Header closeButton>
+//           <Offcanvas.Title className="fs-3 mt-3">
+//             <div>Product Categories</div>
+//           </Offcanvas.Title>
+//         </Offcanvas.Header>
+//         <Offcanvas.Body>
+//           <Link to="/airpodsScreen" className="text-danger text-decoration-none">
+//             AirPods
+//           </Link>
+//           <br></br>
+//           <Link to="/cameraScreen" className="text-danger text-decoration-none">
+//             Camera
+//           </Link>
+//           <br></br>
+//           <Link to="/smartphoneScreen" className="text-danger text-decoration-none">
+//             Smart Phone
+//           </Link>
+//           <div className="mt-5 fs-2">Filter by price</div>
+//           <div className="mt-3">
+//             <Range
+//               step={100}
+//               min={20}
+//               max={10000}
+//               values={priceRange}
+//               onChange={handleRangeChange}
+//               renderTrack={({ props, children }) => (
+//                 <div
+//                   {...props}
+//                   style={{
+//                     ...props.style,
+//                     height: "5px",
+//                     width: "100%",
+//                     backgroundColor: "#ccc",
+//                   }}
+//                 >
+//                   {children}
+//                 </div>
+//               )}
+//               renderThumb={({ props }) => (
+//                 <div
+//                   {...props}
+//                   style={{
+//                     ...props.style,
+//                     height: "17px",
+//                     width: "17px",
+//                     backgroundColor: "black",
+//                     borderRadius: "50%",
+//                   }}
+//                 />
+//               )}
+//             />
+//           </div>
+//           <div>
+//             <div className="float-end mt-4">
+//               ${priceRange[0]} - ${priceRange[1]}
+//             </div>
+//             <button
+//               type="button"
+//               className="btn btn-outline-secondary mt-3 border-start-0"
+//               onClick={handleFilterButtonClick}
+//             >
+//               Filter
+//             </button>
+//           </div>
+//         </Offcanvas.Body>
+//       </Offcanvas>
+//     </>
+//   );
+// }
 
-export default Example;
+// export default Example;
